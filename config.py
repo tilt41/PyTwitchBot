ded905a362d85b04700a31a879daad7d98d5ba51
Tilt <redstoneterror@gmail.com>
2016-02-28 23:39:47 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+#sets variables for connection to twitch chat
+channelname = 'yourname' #Your channel here
+nick = 'botname' #Name of bot here 
+channel = '#'+channelname
+server = 'irc.twitch.tv'
+password = 'replace' #your bot's OAUTH Goes here
+
+psnuname ='PSN'  # Add your psn name here 
