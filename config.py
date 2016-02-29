071a3279c7317efacf895a928383c3bdb3ffed9f
Tilt <redstoneterror@gmail.com>
2016-02-28 23:45:32 -0600

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-#sets variables for connection to twitch chat
-channelname = 'yourname' #Your channel here
-nick = 'botname' #Name of bot here 
-channel = '#'+channelname
-server = 'irc.twitch.tv'
-password = 'replace' #your bot's OAUTH Goes here
-
-psnuname ='PSN'  # Add your psn name here 
